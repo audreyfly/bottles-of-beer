b09688a1ac91907ab11d048413182f024bb5f044
audreyfly <95317631+audreyfly@users.noreply.github.com>
2021-11-30 22:47:59 +0000

    Add files via upload

---
+++ stylesheet.js
@@ -0,0 +1,21 @@
+function Count(){
+	var bottles;
+	var bottlesleft
+	for(i=0;i<=99;i=i+1){
+		
+	
+	document.getElementById(name).InnerHtml+="<br/1>"+i+",";
+	
+	<input type="submit"value=submit>;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
